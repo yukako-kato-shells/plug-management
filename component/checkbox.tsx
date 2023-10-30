cc10e9e873ec003798daa3f71698a9214ad5e289
Yukako Kato <yukako.kato@shells.co.jp>
2023-10-30 15:09:29 +0800

    [feature]インストール画面と、TOP LPの一部を作成

---
+++ component/checkbox.tsx
@@ -0,0 +1,20 @@
+interface CheckBoxProps {
+  checked: boolean;
+  handleChange: () => void;
+  label: string;
+}
+
+const CheckBox: React.FC<CheckBoxProps> = (props) => {
+  return (
+    <label>
+      <input
+        type="checkbox"
+        checked={props.checked}
+        onChange={props.handleChange}
+      />
+      {props.label}
+    </label>
+  )
+}
+
+export default CheckBox;
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
-import ButtonSignInWithSlack from '../../component/ButtonSignInWithSlack';
 import styles from './index.module.css';
+import ButtonWithSlack from '../../component/buttonWithSlack';
 
 const Login: React.FC = () => {
   return (
@@ -11,7 +11,10 @@ const Login: React.FC = () => {
           <div className={styles.title}>
             管理画面に<br />サインインする
           </div>
-          <ButtonSignInWithSlack />
+          <ButtonWithSlack
+            href="https://slack.com/openid/connect/authorize?scope=openid&amp;response_type=code&amp;redirect_uri=https%3A%2F%2Fus-central1-plug-development.cloudfunctions.net%2FauthWithSlack&amp;client_id=6050609319142.6097559104134&amp;state=http://localhost:3000/dashboard"
+            title="Sign in with Slack"
+          />
           <div className={styles.linkStartPlug}>
             これからPlugを始めたい方は<Link href="/"><span>こちら</span></Link>
           </div>
