b072be8be1ce521cbc3169918e16a8f0d82dc871
junya.kawakami <junya.kawakami@shells.co.jp>
2023-12-19 15:04:48 +0900

    [feature] 本番環境以外、検索エンジンにindexされないように対応

--- next.config.js
+++ next.config.js
@@ -2,6 +2,34 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV == 'development' || process.env.NEXT_PUBLIC_VERCEL_ENV == 'staging') {
+      // Development, Staging
+      headers.push({
+        headers: [
+          { key: 'X-Robots-Tag', value: 'noindex' },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ],
+        source: '/:path*',
+      });
+    } else {
+      // Production（localhostも含む　※noindex関係ない）
+      headers.push({
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  }
 }
 
 module.exports = nextConfig
